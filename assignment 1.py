c532d273301bcb492975b89c97349cf5a6bca2e9
sniysh <69708668+sniysh@users.noreply.github.com>
2023-06-14 22:21:21 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,7 @@
+name=input("Enter your name:")
+n1=float(input("Enter n1 marks:"))
+n2=float(input("Enter n2 marks:"))
+n3=float(input("Enter n3 marks:"))
+avg=(n1+n2+n3)/3
+print("Name is:",name)
+print("Avg is:",avg)
