eb8a464fbe3d4014de07b5bb8ba506d1e58662e4
sniysh <69708668+sniysh@users.noreply.github.com>
2023-06-16 16:18:48 +0530

    Add files via upload

---
+++ operator-assign-.py
@@ -0,0 +1,37 @@
+# arithmetic operator
+a=10
+b=5
+print("Addition: ",a+b)
+print("Floor Division: ",a//b)
+
+# assignment operator
+a=10
+a%=1
+print("%=",a)
+a=10
+a**=1
+print("**=",a)
+
+# comparision operator
+a=10
+print(">=",a>=b)
+print("!=",a!=b)
+
+# logical operator
+a=True
+b=False
+print("and",a and b)
+print("or",a or b)
+
+# bitwise operator
+print("bitwise &",a&b)
+print("bitwise <<",a<<b)
+
+# identity operator
+print("is",a is b)
+print("is not",a is not b)
+
+# membership operator
+a="Yash"
+print('s' in a)
+print("a" not in a)
