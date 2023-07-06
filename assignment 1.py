90df2f89a514cd0ca221f3aeed347a0d31367c35
sniysh <69708668+sniysh@users.noreply.github.com>
2023-07-07 01:08:33 +0530

    Update assignment 1.py

--- assignment 1.py
+++ assignment 1.py
@@ -1,7 +1,5 @@
-name=input("Enter your name:")
-n1=float(input("Enter n1 marks:"))
-n2=float(input("Enter n2 marks:"))
-n3=float(input("Enter n3 marks:"))
+n1=float(input('Enter n1: '))
+n2=float(input('Enter n2: '))
+n3=float(input('Enter n3: '))
 avg=(n1+n2+n3)/3
-print("Name is:",name)
-print("Avg is:",avg)
+print('Average of ',n1,',',n2,'and',n3,'is: ',avg )
